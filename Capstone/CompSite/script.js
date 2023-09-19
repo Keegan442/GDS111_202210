0119aecec732ceb9bd96b9c4541fc473e577e033
Keegan Preston <klpreston@email.neit.edu>
2023-09-19 17:39:13 -0400

    commit

---
+++ Capstone/CompSite/script.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.replace("index.html")
+}
+
+function pointCursor() {
+    document.body.style.cursor = "pointer";
+}
+
+function defCursor() {
+    document.body.style.cursor = "default";
+}
